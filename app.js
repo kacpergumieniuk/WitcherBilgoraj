243d6f3b58eb1242376e779f0637a30ed6f99783
Bombsztyl <kacpergumieniuk@gmail.com>
2020-11-30 21:10:14 +0100

    Film section

---
+++ app.js
@@ -0,0 +1,8 @@
+ const filmbutton = document.getElementById("film-button");
+ const filmcontent = document.querySelector(".content");
+
+
+
+ filmbutton.addEventListener('click' , function(){
+     filmcontent.classList.add("comein")
+ })
